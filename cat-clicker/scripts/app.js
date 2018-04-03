73b90c2c3fafe196812a6b9dbb83cd6297e27667
Wisner Júnior <wisnerjr@gmail.com>
2018-04-03 15:03:24 -0300

    Initial dev

--- cat-clicker/scripts/app.js
+++ cat-clicker/scripts/app.js
@@ -1,9 +1,24 @@
-function count() {
-  var p = document.getElementById('cat-count');
-  var value = Number.parseInt(p.textContent);
-  if (! Number.isNaN(value))
-    p.textContent = ++value;
-}  
+class Cat {
+  constructor(name, src) {
+    this.name = name;
+    this.src = src;
+    this.clickCount = 0;
+  }
 
-var elem = document.getElementById('cat');
-elem.addEventListener('click', count);
+  count(elemId) {
+    var p = document.getElementById(elemId);
+    p.textContent = ++this.clickCount;
+  }
+}
+
+var cat1 = new Cat('Meowth', 'img/cat1.jpg');
+var cat2 = new Cat('Persian', 'img/cat2.jpg');
+
+
+var elem1 = document.getElementById('cat-1');
+elem1.src = cat1.src;
+elem1.addEventListener('click', cat1.count('cat-count-1'));
+
+var elem2 = document.getElementById('cat-2');
+elem2.src = cat2.src;
+elem2.addEventListener('click', cat2.count('cat-count-2'));
